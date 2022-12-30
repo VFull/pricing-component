cf690eeed3a6893ba1c8fe446d382bff0841f5e8
Victor Resines <vic.resines@gmail.com>
2022-12-29 20:15:37 -0600

    Add toggle functionality

---
+++ script.js
@@ -0,0 +1,35 @@
+const toggle = document.getElementById("toggle");
+const toggleLabel = document.getElementsByClassName("toggle__label");
+const monthlyPriceBasic = document.getElementById('monthly-price-basic');
+const monthlyPriceProfessional = document.getElementById('monthly-price-professional');
+const monthlyPriceMaster = document.getElementById('monthly-price-master');
+const yearlyPriceBasic = document.getElementById('yearly-price-basic');
+const yearlyPriceProfessional = document.getElementById('yearly-price-professional');
+const yearlyPriceMaster = document.getElementById('yearly-price-master');
+
+//console.log(toggle);
+
+let monthly;
+
+toggle.addEventListener("click", switchToggle);
+
+function switchToggle() {
+  if (toggle.checked) {
+    monthly = false;
+    yearlyPriceBasic.style.display = "block";
+    yearlyPriceProfessional.style.display = "block";
+    yearlyPriceMaster.style.display = "block";
+    monthlyPriceBasic.style.display = "none";
+    monthlyPriceProfessional.style.display = "none";
+    monthlyPriceMaster.style.display = "none";
+  } else {
+    monthly = true;
+    yearlyPriceBasic.style.display = "none";
+    yearlyPriceProfessional.style.display = "none";
+    yearlyPriceMaster.style.display = "none";
+    monthlyPriceBasic.style.display = "block";
+    monthlyPriceProfessional.style.display = "block";
+    monthlyPriceMaster.style.display = "block";
+  }
+  console.log(monthly + "h");
+}
